8db2d23d2c1341a7bf6358759a0358a02729dcaa
Filipi Rafael <filipirafael.123@gmail.com>
2022-12-28 20:59:45 -0300

    Feature
    Add Stack Router for Congrats Screen and delete CartProducts function

--- src/telas/CompraFinalizada/estilos.js
+++ src/telas/CompraFinalizada/estilos.js
@@ -1,5 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titulo: {
+    marginBottom: 30,
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  subtitulo: {
+    marginBottom: 30,
+    fontSize: 18,
+    width: '80%',
+    textAlign: 'center',
+  },
+  botao: {
+    width: 200,
+    paddingVertical: 20
+  }
 });
--- src/telas/CompraFinalizada/index.js
+++ src/telas/CompraFinalizada/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+import { CarProductsContext } from '../../contexts/carproducts';
+
+import Botao from '../../Components/Botao';
 
 import estilos from './estilos';
 
 export default function CompraFinalizada() {
+
+  const navigation = useNavigation();
+  const { setCartProducts } = useContext(CarProductsContext)
+
   return (
-    <></>
+    <View style={estilos.container}>
+      <Text style={estilos.titulo}>Muito obrigado!</Text>
+      <Text style={estilos.subtitulo}>
+        Agradecemos a preferência, seu pedido chegará em breve.
+      </Text>
+      <Botao
+        valor='Retornar aos produtos'
+        estilos={estilos.botao}
+        acao={() => {
+          setCartProducts([]);
+          navigation.navigate('ServicosScreen');
+        }}
+      />
+    </View>
   )
 }
