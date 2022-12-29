fdc61d4bc68cca0cc3e98b6941cd7089f2e2220e
Filipi Rafael <filipirafael.123@gmail.com>
2022-12-28 21:38:07 -0300

    Add animation to cart confirmation screen

--- src/telas/CompraFinalizada/index.js
+++ src/telas/CompraFinalizada/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
@@ -7,15 +7,31 @@ import { CarProductsContext } from '../../contexts/carproducts';
 
 import Botao from '../../Components/Botao';
 
+import LottieView from 'lottie-react-native';
 import estilos from './estilos';
 
 export default function CompraFinalizada() {
 
   const navigation = useNavigation();
-  const { setCartProducts } = useContext(CarProductsContext)
+  const { setCartProducts } = useContext(CarProductsContext);
+  const animation = React.createRef();
+
+  const startAnimation = () => {
+    animation.current.play();
+  }
+
+  useEffect(() => {
+    startAnimation();
+  }, []);
 
   return (
     <View style={estilos.container}>
+      <LottieView
+        source={require('../../assets/animations/cart.json')}
+        style={{ width: 200, height: 200, marginBottom: 20 }}
+        loop={true}
+        ref={animation}
+      />
       <Text style={estilos.titulo}>Muito obrigado!</Text>
       <Text style={estilos.subtitulo}>
         Agradecemos a preferência, seu pedido chegará em breve.
